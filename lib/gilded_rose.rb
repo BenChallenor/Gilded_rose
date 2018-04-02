2d8e1b443896ae81f4cfdd4d495d60777a1976da
Ben <ben.challenor@btopenworld.com>
2018-04-02 14:23:42 +0100

    Added max and min quality constants

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,6 +1,9 @@
 class GildedRose
 
-  attr_reader :items
+  MAX_QUALITY = 50
+  MIN_QUALITY = 0
+
+attr_reader :items
 
 
   def initialize(items)
@@ -10,22 +13,22 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
+        if item.quality > MIN_QUALITY
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
         end
       else
-        if item.quality < 50
+        if item.quality < MAX_QUALITY
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-              if item.quality < 50
+              if item.quality < MAX_QUALITY
                 item.quality = item.quality + 1
               end
             end
             if item.sell_in < 6
-              if item.quality < 50
+              if item.quality < MAX_QUALITY
                 item.quality = item.quality + 1
               end
             end
@@ -38,7 +41,7 @@ class GildedRose
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
+            if item.quality > MIN_QUALITY
               if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality = item.quality - 1
               end
@@ -47,7 +50,7 @@ class GildedRose
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
+          if item.quality < MAX_QUALITY
             item.quality = item.quality + 1
           end
         end
